e316249a6c78ec257fd7720524f217ef5c1b5ea9
John Leonard <jdleonard@vcu.edu>
2024-04-02 10:58:47 -0400

    new html 5th

--- ChatSBSD/App.js
+++ ChatSBSD/App.js
@@ -1,9 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+function Test () {
+  return(
+    <Text style={styles.header1}>Hello from my component</Text>
+  )
+
+  }
+
 export default function App() {
   return (
     <View style={styles.container}>
+      <Test />
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -17,4 +26,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header1: {
+    fontSize: 30,
+    fontWeight: 'bold'
+  }
 });
